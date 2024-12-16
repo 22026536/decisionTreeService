a5e5a631d1d7e4c94a011266bbb39d3426886140
22026536 <22026536>
2024-12-16 17:15:30 +0700

    Update submodule changes

--- decisiontree.py
+++ decisiontree.py
@@ -131,7 +131,7 @@ def train_decision_tree(user_id):
 
     return clf
 
-@app.post('/')
+@app.post('/decisiontree')
 async def recommend_anime(request: Request):
     data = await request.json()
     user_id = data.get("user_id")
